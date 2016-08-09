2857f2f427d5122d7d81dba328e95c69ea721d80
Gavin Mogan <gavin@gavinmogan.com>
2016-08-08 23:17:53 -0700

    no more imagemagick, use graphicsmagic

--- app/icons/build_icons.sh
+++ app/icons/build_icons.sh
@@ -1,3 +1,3 @@
 for size in $(cat ../manifest.webapp | jq --raw-output '.icons | keys | .[]' | xargs); do
-  convert icon.svg -resize ${size}x${size} icon-${size}.png;
+  gm convert icon.svg -resize ${size}x${size} icon-${size}.png;
 done
