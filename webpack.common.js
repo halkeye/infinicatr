49d243f2e3188628b0b39aca3c448f93ea6233bc
Gavin Mogan <git@gavinmogan.com>
2020-11-11 12:26:25 -0800

    use the infinicatr favicon

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 const path = require('path');
 
@@ -107,6 +108,7 @@ module.exports = {
   },
 
   plugins: [
+    new FaviconsWebpackPlugin('./icons/icon.svg'),
     new WorkboxPlugin.GenerateSW({
       clientsClaim: true,
       skipWaiting: true
