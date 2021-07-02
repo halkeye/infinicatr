f3e0972899fc8c53f05034ae6146eb304b5322f3
Gavin Mogan <git@gavinmogan.com>
2021-07-01 19:09:15 -0700

    manifest and service worker

---
+++ rollup-plugin-manifest-json.js
@@ -0,0 +1,45 @@
+import { readFileSync } from 'fs';
+import { basename, resolve, normalize } from 'path';
+
+export default function manifestJson (opts) {
+  let manifestData = {};
+  return {
+    name: 'manifest-json',
+    buildStart () {
+      const { input, manifest } = opts;
+
+      if (!input) {
+        throw new Error('Is the `input` option set to something?');
+      }
+
+      manifestData = JSON.parse(readFileSync(resolve(input), 'utf-8'));
+
+      Object.assign(manifestData, manifest);
+
+      if (manifestData.icons) {
+        for (const icon of manifestData.icons) {
+          icon.referenceId = this.emitFile({
+            type: 'asset',
+            source: readFileSync(resolve(icon.src)),
+            fileName: `manifest_icon_${basename(icon.src)}`
+          });
+        }
+      }
+    },
+    generateBundle () {
+      const { minify, output } = opts;
+      if (manifestData.icons) {
+        for (const icon of manifestData.icons) {
+          const referenceId = icon.referenceId;
+          delete icon.referenceId;
+          icon.src = this.getFileName(referenceId);
+        }
+      }
+      this.emitFile({
+        type: 'asset',
+        source: minify ? JSON.stringify(manifestData) : JSON.stringify(manifestData, null, 2),
+        fileName: output ? normalize(`${output}/manifest.json`) : 'manifest.json'
+      });
+    }
+  };
+}
--- rollup.config.js
+++ rollup.config.js
@@ -3,41 +3,55 @@ import mustache from 'rollup-plugin-mustache';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
-import manifestJson from 'rollup-plugin-manifest-json';
+import manifestJson from './rollup-plugin-manifest-json'; // 'rollup-plugin-manifest-json';
 import html from '@web/rollup-plugin-html';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
-  input: 'app/index.html',
-  output: {
-    dir: 'dist',
-    sourcemap: true
+export default [
+  {
+    input: 'app/js/service-worker',
+    output: {
+      dir: 'dist/sw.js',
+      sourcemap: true
+    },
+    plugins: [
+      production && terser() // minify, but only in production
+    ]
   },
-  plugins: [
-    mustache({
-      include: 'app/templates/*.mustache'
-    }),
-    nodeResolve(), // tells Rollup how to find date-fns in node_modules
-    postcss({
-      plugins: []
-    }),
-    manifestJson({
-      input: 'app/manifest.json', // Required
-      minify: true
-    }),
-    html({
-      injectServiceWorker: true,
-      serviceWorkerPath: './app/js/service-worker.js',
-      transformHtml: [html => {
-        html = html.replace('assets/manifest.json', 'manifest.json');
-        console.log(html);
-        return html;
-      }]
-    }),
-    commonjs(), // converts date-fns to ES modules
-    production && terser() // minify, but only in production
-  ]
-};
+  {
+    input: 'app/index.html',
+    output: {
+      dir: 'dist',
+      sourcemap: true
+    },
+    plugins: [
+      mustache({
+        include: 'app/templates/*.mustache'
+      }),
+      nodeResolve(), // tells Rollup how to find date-fns in node_modules
+      postcss({
+        plugins: []
+      }),
+      manifestJson({
+        input: 'app/manifest.json', // Required
+        minify: true,
+        manifest: {
+          icons: [32, 60, 90, 120, 128, 144, 152, 256, 512].map(size => ({ src: `./app/icons/icon-${size}.png`, sizes: `${size}x${size}`, type: 'image/png' }))
+        }
+      }),
+      html({
+        injectServiceWorker: true,
+        serviceWorkerPath: './dist/sw.js',
+        transformHtml: [html => {
+          html = html.replace('assets/manifest.json', 'manifest.json');
+          return html;
+        }]
+      }),
+      commonjs(), // converts date-fns to ES modules
+      production && terser() // minify, but only in production
+    ]
+  }
+];
